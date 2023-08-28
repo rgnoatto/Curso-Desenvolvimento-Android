da62ec927a3149923bddc547f8099d42e01f2e1a
rgnoa <rgnoatto85@gmail.com>
2023-08-28 20:35:02 -0300

    Versão 1 Clean Codeupdated

--- AppListaCursoRonaldo/app/src/main/java/devandroid/gnoatto/applistacursoronaldo/view/MainActivity.java
+++ AppListaCursoRonaldo/app/src/main/java/devandroid/gnoatto/applistacursoronaldo/view/MainActivity.java
@@ -81,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
         Log.i("POOAndroid", "Objeto pessoa: " + outraPessoa.toString());
 
