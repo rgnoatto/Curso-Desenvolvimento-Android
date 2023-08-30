20df047fa7384ddb9e556003ce55609323bef439
rgnoa <rgnoatto85@gmail.com>
2023-08-29 23:40:47 -0300

    Implementando Log.d na Controller para o método Salvar

--- AppListaCursoRonaldo/app/src/main/java/devandroid/gnoatto/applistacursoronaldo/controller/PessoaController.java
+++ AppListaCursoRonaldo/app/src/main/java/devandroid/gnoatto/applistacursoronaldo/controller/PessoaController.java
@@ -1,4 +1,22 @@
 package devandroid.gnoatto.applistacursoronaldo.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import devandroid.gnoatto.applistacursoronaldo.model.Pessoa;
+
 public class PessoaController {
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC_Controller","Controller iniciada...");
+
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller", "Salvo: "+pessoa.toString());
+    }
 }
